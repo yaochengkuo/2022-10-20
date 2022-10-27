e089e194f0ef0f90562e55db3b0cc5277aaa2d9e
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2022-10-27 16:06:44 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-
+function 燈數 (num: number) {
+    if (num < 6) {
+        x = num - 1
+        y = 0
+    } else if (num >= 6 && num < 9) {
+        x = 4
+        y = num - 5
+    } else if (num >= 9 && num < 14) {
+        x = 13 - num
+        y = 4
+    } else if (num >= 14 && num <= 16) {
+        x = 0
+        y = 17 - num
+    }
+    led.plot(x, y)
+}
+let y = 0
+let x = 0
+let a = 0
+basic.forever(function () {
+    a += 1
+    燈數(a)
+    basic.pause(100)
+})
