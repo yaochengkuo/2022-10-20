4e4b78c7c4bcbd8542d9d575eda3b56cfe1c6e0e
yaochengkuo <115142539+yaochengkuo@users.noreply.github.com>
2022-10-27 16:20:40 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,9 +8,24 @@ function 燈數 (num: number) {
     } else if (num >= 9 && num < 14) {
         x = 13 - num
         y = 4
-    } else if (num >= 14 && num <= 16) {
+    } else if (num >= 14 && num < 16) {
         x = 0
         y = 17 - num
+    } else if (num >= 16 && num < 19) {
+        x = num - 16
+        y = 1
+    } else if (num >= 19 && num <= 21) {
+        x = 3
+        y = num - 18
+    } else if (num > 21 && num <= 23) {
+        x = 24 - num
+        y = 3
+    } else if (num == 24) {
+        x = 1
+        y = 26 - num
+    } else if (num == 25) {
+        x = num - 23
+        y = 2
     }
     led.plot(x, y)
 }
